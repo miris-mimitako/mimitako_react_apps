473cbe52998f9d0a00d26653fd55eec27e30b4d5
miris-mimitako <miris.mikitako@gmail.com>
2022-02-26 23:21:34 +0900

    Updates

---
+++ static/ox4NExN3G6rG86rj6ZHY3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/39f4d8aba4b66723.css","static/chunks/pages/index-5a0ca78db81e36e2.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
