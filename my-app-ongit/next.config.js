693b1e0b8ed965a78162700c1e289f995a863904
miris-mimitako <miris.mikitako@gmail.com>
2022-02-26 23:16:04 +0900

    change: general settings

--- my-app-ongit/next.config.js
+++ my-app-ongit/next.config.js
@@ -3,4 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  distDir: 'build', // this setting is necesarry to using nextjs
+}
